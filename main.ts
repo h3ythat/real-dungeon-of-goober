ef7a60097383be2a5221445cef93cf1bb6c42ff4
h3ythat <Ahyan.Dewan@endeavour.sa.edu.au>
2024-05-24 19:31:22 +0930

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -8,6 +8,9 @@ namespace SpriteKind {
     export const Sign = SpriteKind.create()
     export const arow = SpriteKind.create()
 }
+namespace ConnectionKind {
+    export const door3 = ConnectionKind.create()
+}
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     arrowhere = 0
     if (facing == 1) {
@@ -27,9 +30,35 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
         )
     }
 })
+function makenemy (timetospawn: number) {
+    if (summonnow == 1 && EnemyCount != Enemymax && timetospawn >= 2000) {
+        randomspeed = randint(30, 45)
+        EnemyCount = EnemyCount + 1
+        Onemy = sprites.create(assets.image`myImage4`, SpriteKind.Enemy)
+        onemyspawned = 1
+        Onemy.follow(THEGOOBER, randomspeed)
+    }
+}
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    let playerprojectile: Sprite = null
-    summonProjectile(game.runtime())
+    timed = game.runtime()
+    summonProjectile(timed, idprojectilecounter, 0)
+    idprojectilecounter += 1
+})
+controller.down.onEvent(ControllerButtonEvent.Released, function () {
+    if (facing == 1) {
+        animation.stopAnimation(animation.AnimationTypes.All, THEGOOBER)
+        THEGOOBER.setImage(assets.image`myImage0`)
+    }
+    if (facing == 2) {
+        animation.stopAnimation(animation.AnimationTypes.All, THEGOOBER)
+        THEGOOBER.setImage(assets.image`Goober0`)
+    }
+})
+function summonProjectile (time: number, projectileID: number, done: number) {
+    playerprojectile = sprites.create(assets.image`enemy`, SpriteKind.Projectile)
+    playerprojectile.setFlag(SpriteFlag.DestroyOnWall, true)
+    playerprojectile.setPosition(THEGOOBER.x, THEGOOBER.y)
+    playerprojectile.lifespan = 500
     if (arrowhere == 0) {
         playerprojectile.setVelocity(0, -150)
     }
@@ -42,20 +71,14 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (arrowhere == 3) {
         playerprojectile.setVelocity(-150, 0)
     }
-})
-controller.down.onEvent(ControllerButtonEvent.Released, function () {
-    if (facing == 1) {
-        animation.stopAnimation(animation.AnimationTypes.All, THEGOOBER)
-        THEGOOBER.setImage(assets.image`myImage0`)
-    }
-    if (facing == 2) {
-        animation.stopAnimation(animation.AnimationTypes.All, THEGOOBER)
-        THEGOOBER.setImage(assets.image`Goober0`)
-    }
-})
-function summonProjectile (time: number) {
-	
 }
+tiles.onMapLoaded(function (tilemap3) {
+    if (tiles.getLoadedMap() == tilemap1) {
+    	
+    } else {
+        tiles.placeOnRandomTile(THEGOOBER, assets.tile`doortile`)
+    }
+})
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     arrowhere = 3
     animation.runImageAnimation(
@@ -116,15 +139,46 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile2`, function (sprite, location) {
     buttonpress = 1
 })
+scene.onOverlapTile(SpriteKind.Player, assets.tile`doortile`, function (sprite, location) {
+    tiles.loadConnectedMap(ConnectionKind.door3)
+    THEGOOBER.x += -5
+})
+function KILLLLLLLLL (elemey: Sprite, brojectile: Sprite) {
+	
+}
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite, effects.fire, 500)
+    EnemyCount += -1
+    sprites.setDataNumber(otherSprite, "isdead", 1)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprites.destroy(sprite, effects.disintegrate, 1000)
+    timer.after(500, function () {
+        game.gameOver(false)
+    })
+})
+let SUMMONER = 0
+let timedspawn = 0
+let buttonpress = 0
+let playerprojectile: Sprite = null
+let timed = 0
+let onemyspawned = 0
 let Onemy: Sprite = null
+let randomspeed = 0
 let EnemyCount = 0
 let summonnow = 0
-let SUMMONER = 0
-let randomspeed = 0
-let buttonpress = 0
 let facing = 0
 let arrowhere = 0
 let THEGOOBER: Sprite = null
+let Enemymax = 0
+let idprojectilecounter = 0
+let tilemap1: tiles.WorldMap = null
+let thing = null
+tilemap1 = tiles.createMap(tilemap`level`)
+let tilemap2 = tiles.createMap(tilemap`level6`)
+tiles.connectMapById(tilemap1, tilemap2, ConnectionKind.door3)
+tiles.loadMap(tilemap1)
+idprojectilecounter = 0
 let arrow = sprites.create(assets.image`myImage6`, SpriteKind.arow)
 let tileColumn = 12
 let tileRow = 7
@@ -137,10 +191,9 @@ sign.y = signy
 namespace Enemys {
     
 }
-let Enemymax = 1
+Enemymax = 5
 THEGOOBER = sprites.create(assets.image`Goober1`, SpriteKind.Player)
 controller.moveSprite(THEGOOBER)
-tiles.setCurrentTilemap(tilemap`level`)
 for (let wallz of tiles.getTilesByType(sprites.dungeon.greenOuterNorth0)) {
     tiles.setWallAt(wallz, true)
 }
@@ -181,12 +234,16 @@ game.onUpdate(function () {
     if (arrowhere == 3) {
         arrow.setImage(assets.image`myImage8`)
     }
-    if (THEGOOBER.x >= signx - 20 && THEGOOBER.x <= signx + 20) {
+    if (THEGOOBER.x >= signx - 20 && THEGOOBER.x <= signx + 20 && (THEGOOBER.y >= signy - 20 && THEGOOBER.y <= signy + 20)) {
         sign.sayText(randomspeed)
     } else {
         sign.sayText("")
     }
     if (buttonpress == 1) {
+        if (game.runtime() - timedspawn >= 2000) {
+            timedspawn = game.runtime()
+            makenemy(timedspawn)
+        }
         tiles.setTileAt(tiles.getTileLocation(11, 9), assets.tile`myTile1`)
         SUMMONER = 1
         timer.after(500, function () {
@@ -199,10 +256,5 @@ game.onUpdate(function () {
     }
 })
 game.onUpdateInterval(2000, function () {
-    if (summonnow == 1 && EnemyCount != Enemymax) {
-        randomspeed = randint(30, 45)
-        EnemyCount = EnemyCount + 1
-        Onemy = sprites.create(assets.image`myImage4`, SpriteKind.Enemy)
-        Onemy.follow(THEGOOBER, randomspeed)
-    }
+	
 })
